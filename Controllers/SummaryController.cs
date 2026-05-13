e1fe341b1fd3a5a9d65ce4afc7e5864ca605d3f7
Ananya Jain <ananyaj1515@gmail.com>
2026-05-13 10:45:49 +0800

    Add dashboard for search queries

---
+++ Controllers/SummaryController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class SummaryController : Controller
+    {
+        // GET: SummaryController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
